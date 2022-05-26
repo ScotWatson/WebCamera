9a31f669f019f21053117703df55c7a9a3b65bf4
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-05-25 23:57:46 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -7,14 +7,59 @@ let cameraAPI = ('mediaDevices' in navigator && 'getUserMedia' in navigator.medi
 
 let width;
 let height;
-let myCanvas;
-let myCtx;
+let elemCanvasHidden;
+let ctxHidden;
+let elemCanvasDisplay;
+let ctxDisplay;
 let elemVideo;
 window.addEventListener("load", function () {
+  // x: input data
+  function testTransform(x) {
+    return Math.exp(x);
+  }
+  // funcTransform: a function that takes a single input
+  function createUint8Table(funcTransform) {
+    function calc(element, index, array) {
+      element = funcTransform(index);
+    }
+    const ret = new Uint8Array(256);
+    ret.forEach(calc);
+    return ret;
+  }
+  // data: Uint8Array
+  // tblTransform: Uint8Array
+  function transformUint8(data, tblTransform) {
+    function calc(element, index, array) {
+      return tblTransform[element];
+    }
+    return data.map(calc);
+  }
+  function testPerformance(numDataSize, numIterations) {
+    const tblTransform = createUint8Table(testTransform);
+    const data = new Uint8Array(numDataSize);
+    let timeAcc = 0;
+    for (let i = 0; i < numIterations; ++i) {
+      crypto.getRandomValues(data);
+      const timeStart = Performance.now();
+      let result = transformUint8(data, tblTransform);
+      const timeEnd = Performance.now();
+      const timeElapsed = (timeEnd - timeStart);
+      timeAcc += timeElapsed;
+    }
+    console.log("Data size: ", numDataSize, "  Avg time: ", (timeAcc / numIterations));
+  }
+  function samplePerformance() {
+    testPerformance(10, 1000);
+    testPerformance(20, 1000);
+    testPerformance(50, 1000);
+    testPerformance(100, 1000);
+    testPerformance(200, 1000);
+    testPerformance(500, 1000);
+  }
   if (cameraAPI) {
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
-    let promiseCamera = navigator.mediaDevices.getUserMedia({video: true});
+    const promiseCamera = navigator.mediaDevices.getUserMedia({video: true});
     promiseCamera.then(function(stream) {
       elemVideo = document.createElement("video");
       elemVideo.style.display = "none";
@@ -22,11 +67,16 @@ window.addEventListener("load", function () {
       document.body.appendChild(elemVideo);
       elemVideo.play();
       elemVideo.addEventListener("play", function () {
-        myCanvas = document.createElement("canvas");
-        myCanvas.style.border = "none";
-        document.body.appendChild(myCanvas);
-        myCtx = myCanvas.getContext("2d");
-        myCtx.clearRect(0, 0, width, height);
+        elemCanvasHidden = document.createElement("canvas");
+        elemCanvasHidden.style.display = "none";
+        document.body.appendChild(elemCanvasHidden);
+        ctxHidden = elemCanvasHidden.getContext("2d");
+        ctxHidden.clearRect(0, 0, width, height);
+        elemCanvasDisplay = document.createElement("canvas");
+        elemCanvasDisplay.style.border = "none";
+        document.body.appendChild(elemCanvasDisplay);
+        ctxDisplay = elemCanvasDisplay.getContext("2d");
+        ctxDisplay.clearRect(0, 0, width, height);
         requestAnimationFrame(parseFrame);
       });
     });
@@ -39,7 +89,6 @@ window.addEventListener("load", function () {
     document.body.appendChild(textMsg);
   }
   function parseFrame() {
-    console.log("frame");
     let resized = false;
     if (width != elemVideo.videoWidth) {
       width = elemVideo.videoWidth;
@@ -50,12 +99,22 @@ window.addEventListener("load", function () {
       resized = true;
     }
     if (resized) {
-      myCanvas.width = width;
-      myCanvas.height = height;
-      myCanvas.style.width = width + "px";
-      myCanvas.style.height = height + "px";
+      elemCanvasHidden.width = width;
+      elemCanvasHidden.height = height;
+      resizeCanvasDisplay(width, height);
     }
-    myCtx.drawImage(elemVideo, 0, 0, width, height);
+    ctxHidden.drawImage(elemVideo, 0, 0, width, height);
+    const data = ctxHidden.getImageData(0, 0, width, height);
+    ctxDisplay.drawImage(elemCanvasHidden, 0, 0, width, height);
     requestAnimationFrame(parseFrame);
+  }
+  function resizeCanvasDisplay(width, height) {
+    elemCanvasDisplay.width = width;
+    elemCanvasDisplay.height = height;
+    elemCanvasDisplay.style.width = width + "px";
+    elemCanvasDisplay.style.height = height + "px";
+  }
+  function recalcCanvasDisplay() {
+    ctxDisplay.drawImage(elemCanvasHidden, 0, 0, width, height);
   }
 });
