ed1cf87b7dfb20a556e6977d88878f79f6a793d0
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-05-26 20:34:28 -0400

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -63,20 +63,40 @@ window.addEventListener("load", function () {
     ctxHidden.drawImage(elemVideo, 0, 0, width, height);
     frameData = ctxHidden.getImageData(0, 0, width, height);
   }
+  // x: r, g, or b value; 0-255 inclusive
+  // Returns: linearized, normalized intensity
+  function sRGB_to_linear(x) {
+    const norm_x = x / 255;
+    if (norm_x <= 0.04045) {
+      return (norm_x / 12.92);
+    } else {
+      return Math.pow((norm_x + 0.055) / 1.055, 2.4);
+    }
+  }
+  const tblGammaLinear = createUint8Float64Table(sRGB_to_linear);
+  let arrIntensity;
   function parseFrameData() {
     const thisData = frameData.data;
-    const vecIntensityMult = [ 0.2, 0.8, 0.1 ];
-    arrIntensity
+    const vecIntensityMult = new Float64Array(3);
+    vecIntensityMult[0] = 0.2;
+    vecIntensityMult[1] = 0.7;
+    vecIntensityMult[2] = 0.1;
+    arrIntensity = new Float64Array(width * height);
     for (let i = 0; i < height; ++i) {
       for (let j = 0; j < width; ++j) {
-        const vec1 = thisData.slice( 4 * (i * width + j), 4 * (i * width + j) + 3 );
-        
-        const val = dotProductUint8Float(vec1, vecIntensityMult);
+        const numIndex = (i * width + j);
+        const vecColor = thisData.slice( 4 * numIndex, 4 * numIndex + 3 );
+        const vecColorLinear = transformUint8Float64(vecColor, tblGammaLinear);
+        arrIntensity[numIndex] = dotProductUint8Float64(vecColorLinear, vecIntensityMult);
       }
     }
   }
   function parseFrame() {
     captureFrame();
+    const start_time = performance.now();
+    parseFrameData();
+    const end_time = performance.now();
+    console.log("Calc time: ", end_time - start_time, "ms");
     resizeCanvasDisplay(width, height);
     ctxDisplay.putImageData(frameData, 0, 0);
     requestAnimationFrame(parseFrame);
