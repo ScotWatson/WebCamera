4c582608eebbc7161d74f17d1ce0856742e60443
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-05-28 07:47:28 -0400

    Update math.js

--- docs/math.js
+++ docs/math.js
@@ -393,14 +393,14 @@ function samplePerformance() {
   console.log("matrixProductUint8Uint8_1");
   testPerformanceMatrixProductUint8Uint8_1(10, 1000);
   testPerformanceMatrixProductUint8Uint8_1(20, 1000);
-  testPerformanceMatrixProductUint8Uint8_1(50, 1);
-  testPerformanceMatrixProductUint8Uint8_1(100, 1);
-  testPerformanceMatrixProductUint8Uint8_1(200, 1);
+  testPerformanceMatrixProductUint8Uint8_1(50, 100);
+  testPerformanceMatrixProductUint8Uint8_1(100, 100);
+  testPerformanceMatrixProductUint8Uint8_1(200, 10);
   console.log("matrixProductUint8Uint8_2");
-  testPerformanceMatrixProductUint8Uint8_2(10, 1);
-  testPerformanceMatrixProductUint8Uint8_2(20, 1);
-  testPerformanceMatrixProductUint8Uint8_2(50, 1);
-  testPerformanceMatrixProductUint8Uint8_2(100, 1);
-  testPerformanceMatrixProductUint8Uint8_2(200, 1);
+  testPerformanceMatrixProductUint8Uint8_2(10, 1000);
+  testPerformanceMatrixProductUint8Uint8_2(20, 1000);
+  testPerformanceMatrixProductUint8Uint8_2(50, 100);
+  testPerformanceMatrixProductUint8Uint8_2(100, 100);
+  testPerformanceMatrixProductUint8Uint8_2(200, 10);
 }
 samplePerformance();
