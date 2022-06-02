2d84e34b2119b7db5f7dc2d5a40a892c68367740
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2022-06-02 00:32:52 -0400

    Update math.js

--- docs/math.js
+++ docs/math.js
@@ -74,259 +74,206 @@ const MatrixMath = (function () {
   }
   objMain.definiteLoop = newOptionFunction(definiteLoop_52D92525);
 
+  // Javascript supports various different typed arrays:
+  // Int8Array
+  // Uint8Array
+  // Uint8ClampedArray
+  // Int16Array
+  // Uint16Array
+  // Int32Array
+  // Uint32Array
+  // Float32Array
+  // Float64Array
+  // BigInt64Array
+  // BigUint64Array
+
   // Fills a buffer with random values
-  // typedArray: a TypedArray that will be filled with random values
+  // outputArray: a TypedArray that will be filled with random values
   // Returns: undefined
-  function getRandomValues_CCD8F9ED = function (typedArray) {
+  function getRandomArray_CCD8F9ED(outputArray) {
     const maxLength = 65536 / typedArray.BYTES_PER_ELEMENT;
     const numLoops = typedArray.length / maxLength;
     for (let i = 0; i < numLoops; ++i) {
       crypto.getRandomValues(typedArray.subarray(maxLength * i, maxLength * (i + 1)));
     }
   }
-  objMain.getRandomValues = newOptionFunction(getRandomValues_CCD8F9ED);
+  objMain.getRandomArray = newOptionFunction(getRandomArray_CCD8F9ED);
 
   // Remove column
-  // data: UintArray containing the data to be copied
+  // outputArray: TypedArray (numRows = data.length / numCols; .length === numRows * (numCols - 1))
+  // data: TypedArray containing the data to be copied
   // numCols: total number of columns
   // colToRemove: index of column to be removed
-  // Returns: Uint8Array
-  function matrixRemoveColumnUint8_55B7A225(data, numCols, colToRemove) {
+  // Returns: undefined
+  function matrixRemoveColumn_55B7A225(outputArray, data, numCols, colToRemove) {
     const numRows = data.length / numCols;
-    const ret = new Uint8Array(numRows * (numCols - 1));
     let retIndex = 0;
-    definiteLoop(copy, data.length);
-    return ret;
-    function copy(index) {
+    for (let index = 0; index < data.length; ++i) {
       if ((index % numCols) != colToRemove) {
         ret[retIndex] = data[index];
         ++retIndex;
       }
     }
   }
-  objMain.matrixRemoveColumnUint8 = newOptionFunction(matrixRemoveColumnUint8_55B7A225);
+  objMain.matrixRemoveColumn = newOptionFunction(matrixRemoveColumn_55B7A225);
 
   // Memoization function - Calculates for all integers 0 to 255 (inclusive)
+  // outputArray: TypedArray (.length === 256)
   // funcTransform: A function that takes a single argument (Number) and returns (Number)
-  // Returns: Uint8Array (length == 256)
-  function createUint8Uint8Table_CA07DBBA(funcTransform) {
-    function calc(element, index, array) {
-      element = funcTransform(index);
+  // Returns: undefined
+  function createTableForUint8_CA07DBBA(outputArray, funcTransform) {
+    for (let index = 0, i < 256; ++i) {
+      outputArray[index] = funcTransform(index);
     }
-    const ret = new Uint8Array(256);
-    ret.forEach(calc);
-    return ret;
   }
-  objMain.createUint8Uint8Table = newOptionFunction(createUint8Uint8Table_CA07DBBA);
+  objMain.createTableForUint8 = newOptionFunction(createTableForUint8_CA07DBBA);
 
-  // Memoization function - Calculates for all integers 0 to 255 (inclusive)
+  // Memoization function - Calculates for all integers -128 to 127 (inclusive)
+  // outputArray: TypedArray (.length === 256)
   // funcTransform: A function that takes a single argument (Number) and returns (Number)
-  // Returns: Float32Array (length == 256), 1 kiB
-  function createUint8Float32Table_A42DFA1E(funcTransform) {
-    function calc(element, index, array) {
-      element = funcTransform(index);
+  // Returns: undefined
+  function createTableForInt8_A42DFA1E(funcTransform) {
+    for (let index = 0, i < 256; ++i) {
+      outputArray[index] = funcTransform(index - 128);
     }
-    const ret = new Float32Array(256);
-    ret.forEach(calc);
-    return ret;
   }
-  objMain.createUint8Float32Table = newOptionFunction(createUint8Float32Table_A42DFA1E);
-
-  // Memoization function - Calculates for all integers 0 to 255 (inclusive)
-  // funcTransform: A function that takes a single argument (Number) and returns (Number)
-  // Returns: Float64Array (length == 256)
-  function createUint8Float64Table_4830329E(funcTransform) {
-    function calc(element, index, array) {
-      element = funcTransform(index);
-    }
-    const ret = new Float64Array(256);
-    ret.forEach(calc);
-    return ret;
-  }
-  objMain.createUint8Float64Table = newOptionFunction(createUint8Float64Table_4830329E);
+  objMain.createTableForInt8 = newOptionFunction(createTableForInt8_A42DFA1E);
 
   // Uses tblTransform to transform each element of input
+  // outputArray: TypedArray (.length === input.length)
   // input: Uint8Array
-  // tblTransform: Uint8Array, must have length == 256
-  function transformUint8Uint8_F50AD938(input, tblTransform) {
-    function calc(element, index, array) {
-      return tblTransform[element];
+  // tblTransform: TypedArray (.length == 256)
+  function transformFromUint8_F50AD938(outputArray, input, tblTransform) {
+    for (let index = 0; index < input.length; ++index) {
+      outputArray[index] = tblTransform[input[index]];
     }
-    return input.map(calc);
   }
-  objMain.transformUint8Uint8 = newOptionFunction(transformUint8Uint8_F50AD938);
+  objMain.transformFromUint8 = newOptionFunction(transformFromUint8_F50AD938);
 
   // Uses tblTransform to transform each element of input
+  // outputArray: TypedArray (.length === input.length)
   // input: Uint8Array
-  // tblTransform: Uint8Array, must have length == 256
-  function transformFloat64Uint8_DB586F99(input, tblTransform) {
-    function calc(elementInput, index, array) {
-      return (tblTransform.findIndex(gt) - 1);
-      function gt(elementTransform, index, array) {
-        return (elementInput >= elementTransform);
+  // tblTransform: Uint8Array (.length == 256)
+  function transformToUint8_DB586F99(outputArray, input, tblTransform) {
+    for (let index = 0; index < input.length; ++index) {
+      for (let tableIndex = 1; tableIndex < tblTransform.length; ++index) {
+        if (input[index] > tblTransform[tableIndex]) {
+          outputArray[index] = index - 1;
+          break;
+        }
       }
     }
-    return input.map(calc);
   }
-  objMain.transformFloat64Uint8 = newOptionFunction(transformFloat64Uint8_DB586F99);
+  objMain.transformToUint8 = newOptionFunction(transformToUint8_DB586F99);
 
   // Uses tblTransform to transform each element of input
-  // data: Uint8Array
-  // tblTransform: Float32Array, must have length == 256
-  function transformUint8Float32_20B227EF(input, tblTransform) {
-    function calc(element, index, array) {
-      return tblTransform[element];
+  // outputArray: TypedArray (.length === input.length)
+  // input: Int8Array
+  // tblTransform: TypedArray (.length == 256)
+  function transformFromInt8_20B227EF(outputArray, input, tblTransform) {
+    for (let index = 0; index < input.length; ++index) {
+      outputArray[index] = tblTransform[input[index] + 128];
     }
-    return input.map(calc);
   }
-  objMain.transformUint8Float32 = newOptionFunction(transformUint8Float32_20B227EF);
+  objMain.transformFromInt8 = newOptionFunction(transformFromInt8_20B227EF);
 
   // Uses tblTransform to transform each element of input
-  // data: Uint8Array
-  // tblTransform: Float64Array, must have length == 256
-  function transformUint8Float64_F6A7E433(input, tblTransform) {
-    function calc(element, index, array) {
-      return tblTransform[element];
+  // outputArray: TypedArray (.length === input.length)
+  // input: Uint8Array
+  // tblTransform: Uint8Array (.length == 256)
+  function transformToInt8_F6A7E433(outputArray, input, tblTransform) {
+    for (let index = 0; index < input.length; ++index) {
+      for (let tableIndex = 1; tableIndex < tblTransform.length; ++index) {
+        if (input[index] > tblTransform[tableIndex]) {
+          outputArray[index] = (index - 1) - 128;
+          break;
+        }
+      }
     }
-    return input.map(calc);
   }
-  objMain.transformUint8Float64 = newOptionFunction(transformUint8Float64_F6A7E433);
+  objMain.transformToInt8 = newOptionFunction(transformToInt8_F6A7E433);
 
   // Calculates the dot product of vec1 & vec2
-  // vec1: Uint8Array
-  // vec2: Uint8Array
-  // vec1 & vec2 must be the same length
+  // vec1: TypedArray
+  // vec2: TypedArray
+  // (vec1.length === vec2.length)
   // Returns: Number
-  function dotProductUint8Uint8_2377B9AD(vec1, vec2) {
-    function calc(acc, element, index, array) {
-      return (acc + (element * vec2[index]));
-    }
-    return vec1.reduce(calc, 0);
-  }
-  objMain.dotProductUint8Uint8 = newOptionFunction(dotProductUint8Uint8_2377B9AD);
-  function dotProductUint8Uint8_66A6E181(vec1, vec2) {
+  function dotProduct_2377B9AD(vec1, vec2) {
     let acc = 0;
-    definiteLoop(calc, vec1.length);
-    function calc(index) {
+    for (let index = 0; index < vec1.length; ++index) {
       acc += vec1[index] * vec2[index];
     }
     return acc;
   }
-  objMain.dotProductUint8Uint8.addOption(dotProductUint8Uint8_66A6E181);
+  objMain.dotProduct = newOptionFunction(dotProduct_2377B9AD);
   
-  // Calculates the dot product of vec1 & vec2
-  // vec1: Uint8Array
-  // vec2: Float32Array
-  // vec1 & vec2 must be the same length
-  // Returns: Float32Array (length == 1)
-  function dotProductUint8Float32_8930D832(vec1, vec2) {
-    function calc(acc, element, index, array) {
-      acc[0] += element * vec2[index];
-      return acc;
-    }
-    return vec1.reduce(calc, new Float32Array(1));
-  }
-  objMain.dotProductUint8Float32 = newOptionFunction(dotProductUint8Float32_8930D832);
-
-  // Calculates the dot product of vec1 & vec2
-  // vec1: Uint8Array
-  // vec2: Float64Array
-  // vec1 & vec2 must be the same length
-  // Returns: Float64Array (length == 1)
-  function dotProductUint8Float64_322293FE(vec1, vec2) {
-    function calc(acc, element, index, array) {
-      acc[0] += element * vec2[index];
-      return acc;
-    }
-    return vec1.reduce(calc, new Float64Array(1));
-  }
-  objMain.dotProductUint8Float64 = newOptionFunction(dotProductUint8Float64_322293FE);
-
   // Calculates the matrix product
-  // rows: Uint8Array
-  // numCols: length of each row in rows
-  // cols: Uint8Array
-  // numRows: length of each column in cols
-  // Returns: Uint16Array; length == rows.length * cols.length / (numRowsCols * numRowsCols)
-  function matrixProductUint8Uint8_3AB43895(rows, cols, numColsRows) {
-    const numRows = (rows.length / numColsRows);
-    const arrRows = new Array(numRows);
-    for (let i = 0; i < numRows; ++i) {
-      arrRows[i] = rows.subarray(numColsRows * i, numColsRows * (i + 1));
-    }
-    const numCols = (cols.length / numColsRows);
-    const arrCols = new Array(numCols);
-    for (let i = 0; i < numCols; ++i) {
-      arrCols[i] = cols.subarray(numColsRows * i, numColsRows * (i + 1));
-    }
-    let ret = new Uint16Array(arrRows.length * arrCols.length);
-    for (let i = 0; i < numRows; ++i) {
-      for (let j = 0; j < numCols; ++j) {
-        ret[numCols * i + j] = dotProductUint8Uint8(arrRows[i], arrCols[j]);
-      }
-    }
-    return ret;
-  }
-  objMain.matrixProductUint8Uint8 = newOptionFunction(matrixProductUint8Uint8_3AB43895);
-  function matrixProductUint8Uint8_4AB3AD62(rows, cols, numColsRows) {
+  // outputArray: TypedArray ( .length == rows.length * cols.length / (numRowsCols * numRowsCols) )
+  // rows: TypedArray
+  // cols: TypedArray
+  // numColsRows: length of each row in rows, length of each column in cols (must be equal)
+  // Returns: undefined
+  function matrixProduct_3AB43895(outputArray, rows, cols, numColsRows) {
     const numRows = (rows.length / numColsRows);
     const numCols = (cols.length / numColsRows);
-    let ret = new Uint16Array(numRows * numCols);
     for (let i = 0; i < numRows; ++i) {
       for (let j = 0; j < numCols; ++j) {
-        ret[numCols * i + j] = 0;
+        outputArray[numCols * i + j] = 0;
         for (let k = 0; k < numColsRows; ++k) {
-          ret[numCols * i + j] += (rows[i * numColsRows + k] + cols[j * numColsRows + k]);
+          outputArray[numCols * i + j] += (rows[i * numColsRows + k] + cols[j * numColsRows + k]);
         }
       }
     }
-    return ret;
   }
-  objMain.matrixProductUint8Uint8.addOption(matrixProductUint8Uint8_4AB3AD62);
+  objMain.matrixProduct = newOptionFunction(matrixProduct_3AB43895);
 
-  // Calculates the matrix product
-  // rows: Uint8Array
-  // numCols: length of each row in rows
-  // cols: Float64Array
-  // numRows: length of each column in cols
-  // Returns: Float64Array; length == rows.length * cols.length / (numRowsCols * numRowsCols)
-  function matrixProductUint8Float64_7BBE6F53(rows, cols, numColsRows) {
-    const numRows = (rows.length / numColsRows);
-    const numCols = (cols.length / numColsRows);
-    let ret = new Float64Array(numRows * numCols);
-    for (let i = 0; i < numRows; ++i) {
-      for (let j = 0; j < numCols; ++j) {
-        ret[numCols * i + j] = 0;
-        for (let k = 0; k < numColsRows; ++k) {
-          ret[numCols * i + j] += (rows[i * numColsRows + k] + cols[j * numColsRows + k]);
-        }
+  // Extracts a portion of a matrix
+  // outputArray: TypedArray ( .length == newRows * numCols )
+  // input: TypedArray
+  // numCols: length of each row in input
+  // startRow: row index of new upper left corner
+  // startCol: column index of new upper left corner
+  // newRows: number of rows in the sub-matrix
+  // newCols: number of columns in the sub-matrix
+  // Returns: undefined
+  function subMatrix_D209E63C(outputArray, input, numCols, startRow, startCol, newRows, newCols) {
+    if (startCol + newCols > numCols) {
+      throw new Error("Invalid index");
+    }
+    const numRows = (input.length / numCols);
+    if (startRow + newRows > numRows) {
+      throw new Error("Invalid index");
+    }
+    for (let i = startRow; i < startRow + newRows; ++i) {
+      for (let j = startCol; j < startCol + newCols; ++j) {
+        outputArray[newCols * i + j] = input[numCols * i + j];
       }
     }
-    return ret;
   }
-  objMain.matrixProductUint8Float64 = newOptionFunction(matrixProductUint8Float64_7BBE6F53);
+  objMain.subMatrix = newOptionFunction(subMatrix_D209E63C);
 
-  // Calculates the matrix product
-  // rows: Float64Array
-  // numCols: length of each row in rows
-  // cols: Float64Array
-  // numRows: length of each column in cols
-  // Returns: Float64Array; length == rows.length * cols.length / (numRowsCols * numRowsCols)
-  function matrixProductFloat64Float64_23FA6ABA(rows, cols, numColsRows) {
-    const numRows = (rows.length / numColsRows);
-    const numCols = (cols.length / numColsRows);
-    let ret = new Float64Array(numRows * numCols);
-    for (let i = 0; i < numRows; ++i) {
-      for (let j = 0; j < numCols; ++j) {
-        ret[numCols * i + j] = 0;
-        for (let k = 0; k < numColsRows; ++k) {
-          ret[numCols * i + j] += (rows[i * numColsRows + k] + cols[j * numColsRows + k]);
-        }
+  // Convolves a matrix with a kernel
+  // outputArray: TypedArray ( .length == (inputRows - kernelRows + 1) * (inputCols - numCols + 1) )
+  // input: TypedArray
+  // inputCols: length of each row in input
+  // kernel: TypedArray
+  // kernelCols: length of each row in kernel
+  // Returns: undefined
+  function convolve_87DC2DF4(outputArray, input, inputCols, kernel, kernelCols) {
+    if (startCol + newCols > numCols) {
+      throw new Error("Invalid index");
+    }
+    const numRows = (input.length / numCols);
+    if (startRow + newRows > numRows) {
+      throw new Error("Invalid index");
+    }
+    for (let i = startRow; i < startRow + newRows; ++i) {
+      for (let j = startCol; j < startCol + newCols; ++j) {
+        outputArray[newCols * i + j] = input[numCols * i + j];
       }
     }
-    return ret;
   }
-  objMain.matrixProductFloat64Float64 = newOptionFunction(matrixProductFloat64Float64_23FA6ABA);
+  objMain.convolve = newOptionFunction(convolve_87DC2DF4);
 
   return objMain;
 })();
